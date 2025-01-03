a01c6ac1fa53469e93f5dbe7cad2062c719a11f2
kapoylawas <arif.sangga@gmail.com>
2025-01-03 14:09:53 +0700

    update new

---
+++ src/routes/index.jsx
@@ -0,0 +1,29 @@
+//import react router dom
+import { Routes, Route, Navigate } from "react-router-dom";
+
+//import store
+import { useStore } from "../stores/user";
+
+//import view login
+import Login from "../views/auth/login.jsx";
+import Dashboard from "../views/dashboard/index.jsx";
+
+export default function AppRoutes() {
+  //destruct state "token" from store
+  const { token } = useStore();
+
+  return (
+    <Routes>
+      {/* route "/" */}
+      <Route
+        path="/"
+        element={token ? <Navigate to="/dashboard" replace /> : <Login />}
+      />
+      {/* route "/dashboard" */}
+      <Route
+        path="/dashboard"
+        element={token ? <Dashboard /> : <Navigate to="/" replace />}
+      />
+    </Routes>
+  );
+}
---
+++ src/views/auth/login.jsx
@@ -0,0 +1,134 @@
+//import layout auth
+import LayoutAuth from "../../layouts/auth";
+
+//import hook react
+import React, { useState } from "react";
+
+//import hook useNavigate from react router dom
+import { useNavigate } from "react-router-dom";
+
+//import store
+import { useStore } from "../../stores/user";
+
+//import handler error
+import { handleErrors } from "../../utils/handleErrors";
+
+export default function Login() {
+  //navigate
+  const navigate = useNavigate();
+
+  //destruct action "login" from store
+  const { login } = useStore();
+
+  //state login loading
+  const [loading, setLoading] = useState(false);
+
+  //define state
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  console.log("email ->", email);
+  console.log("password ->", password);
+
+  //define state errors
+  const [errors, setErrors] = useState({});
+  const [loginFailed, setLoginFailed] = useState("");
+
+  //function "loginHanlder"
+  const loginHandler = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    try {
+      // Call action "login" from store
+      await login({ email, password });
+
+      // Redirect to dashboard
+      navigate("/dashboard");
+
+      // Disable loading after a short delay (if needed)
+      setTimeout(() => {
+        setLoading(false);
+      }, 2000);
+    } catch (error) {
+      if (error.response.data.message) {
+        setLoginFailed(error.response.data.message);
+      } else {
+        // Assign error to function "handleErrors"
+        handleErrors(error.response.data, setErrors);
+      }
+
+      // Ensure loading is disabled in case of error
+      setLoading(false);
+    }
+  };
+
+  return (
+    <LayoutAuth>
+      <div className="text-center mb-4 mt-5">
+        <a
+          href="/"
+          className="navbar-brand navbar-brand-autodark p-4 bg-blue-lt rounded-circle shadow-sm"
+        >
+          <img src="/images/logo.png" height="60" alt="" />
+        </a>
+        <br />
+        <h2 className="mt-3">CASHIER APPS</h2>
+      </div>
+      <div className="card card-md rounded">
+        <div className="card-body">
+          <h2 className="h2 text-center mb-4">Login to your account</h2>
+          {loginFailed && (
+            <div className="alert alert-danger mt-2">{loginFailed}</div>
+          )}
+          <form onSubmit={loginHandler} autoComplete="off" noValidate>
+            <div className="mb-3">
+              <label className="form-label">Email address</label>
+              <input
+                type="email"
+                className="form-control"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                placeholder="your@email.com"
+                autoComplete="off"
+              />
+              {errors.email && (
+                <div className="alert alert-danger mt-2">{errors.email}</div>
+              )}
+            </div>
+            <div className="mb-2">
+              <label className="form-label">Password</label>
+              <input
+                type="password"
+                className="form-control"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Your password"
+                autoComplete="off"
+              />
+              {errors.password && (
+                <div className="alert alert-danger mt-2">{errors.password}</div>
+              )}
+            </div>
+            <div className="mb-2">
+              <label className="form-check">
+                <input type="checkbox" className="form-check-input" />
+                <span className="form-check-label">
+                  Remember me on this device
+                </span>
+              </label>
+            </div>
+            <div className="form-footer">
+              <button
+                type="submit"
+                className="btn btn-primary w-100 rounded"
+                disabled={loading}
+              >
+                {loading ? "loading..." : "Sign in"}
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </LayoutAuth>
+  );
+}
